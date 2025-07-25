d894acaa5287189a8bbb88de3fc8dbc175ca2c9f
Maricheck <faizushaikh0220@gmail.com>
2025-07-25 18:52:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.sqlite3'
+
+db = SQLAlchemy(app)
+
 @app.route('/')
 def home():
-    return "Hello from Flask!"
+    return 'Flask is working with database!'
